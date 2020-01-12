f14f9a6079b82d34b0230bf5399d05f1dfea015d
Ben Olsen <olsenben1992@gmail.com>
2020-01-11 16:30:22 -0800

    display changes

--- script.js
+++ script.js
@@ -3,46 +3,59 @@ $("button").on("click", function() {
     .val()
     .trim();
   getWeather(weather);
-  // getForecast(weather);
-  // getUV(weather);
 });
 
 function getWeather(weather) {
-  //console.log("weather " + weather);
   var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     weather +
     "&appid=a8b347e5b5e166be903a16942ba5219c";
-  //   console.log(queryURL);
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    var results = response.data;
     console.log(response);
-    console.log("temperature " + kelvinToF(response.main.temp));
+    console.log("temperature " + kelvinToF(response.main.temp).toPrecision(3));
     console.log("humidity " + response.main.humidity);
     console.log("wind speed  " + response.wind.speed);
-    console.log(text(data.name + " (" + new Date().toLocaleDateString() + ")"));
+    // console.log(data.name + " (" + new Date().toLocaleDateString() + ")");
 
-    var weatherDiv = $("<div>");
+    var weatherDiv = $("<p>");
+    weatherDiv.text(
+      "temperature " +
+        kelvinToF(response.main.temp) +
+        "°" +
+        (" humidty " + response.main.humidity) +
+        (" wind speed " + response.wind.speed)
+    );
+    weatherDiv.text;
+    //var forecastImage = $("<img>");
 
-    //var p = $("<p>").text("Temperature: " + results[i].temperature);
+    //weatherDiv.append(forecastImage);
 
-    var forecastImage = $("<img>");
-
-    //forecastImage.attr("src", results[i].images.fixed_height.url);
-
-    // weatherDiv.append(p);
-    weatherDiv.append(forecastImage);
-
-    $("#weather-forecast-here").prepend(weatherDiv);
+    $(".container-xl").prepend(weatherDiv);
   });
 }
+function getUVI(lat, lon) {
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" +
+    lat +
+    "&lon=" +
+    lon;
+  //"http://api.openweathermap.org/data/2.5/uvi?appid=a8b347e5b5e166be903a16942ba5219c&lat=37.75&lon=122.37";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log("temperature " + kelvinToF(response.main.temp));
+    console.log(response);
+  });
+}
+
+// getUVI();
 
 function kelvinToF(temp) {
   return (temp - 273.15) * (9 / 5) + 32;
 }
 console.log(kelvinToF(285));
-document.getElementById("search-bar").value = localStorage.getItem(weather);
